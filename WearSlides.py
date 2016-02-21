f9351f48f3cc2d93a106906bd5783880f97400b1
Konrad Iturbe <github@chernowii.com>
2016-02-21 16:52:27 +0100

    Fixed python script (Windows only)

--- WearSlides.py
+++ WearSlides.py
@@ -16,16 +16,13 @@ def index():
 def nextkey():
     ctypes.windll.user32.keybd_event(0x27, 0, 0, 0) #A is down
     ctypes.windll.user32.keybd_event(0x27, 0, 0x0002, 0) #A is up
-    print("Right key")
     return 'OK'
 
 @app.route('/prev')
 def prevkey():
     ctypes.windll.user32.keybd_event(0x25, 0, 0, 0) #A is down
     ctypes.windll.user32.keybd_event(0x25, 0, 0x0002, 0) #A is up
-    print("Left key")
     return 'OK'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
